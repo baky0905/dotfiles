8acb2b2ca5e815b10d04418811a1135bf684d269
Kristijan Bakaric <kbakaric1@gmail.com>
2022-11-02 21:37:16 +0100

    fix paths

--- home/.bash.d/paths.sh
+++ home/.bash.d/paths.sh
@@ -18,14 +18,11 @@ bash_tools="${HOME}/.dotfiles/home"
 # unreliable that HOME is set, ensure shell evaluates to the right thing before we use it
 [[ -n "${HOME:-}" ]] || HOME=~
 
-github="${github:-${HOME}/github}"
-
 # shellcheck disable=SC1090
 . "${bash_tools}/.bash.d/os_detection.sh"
 
 # ============================================================================ #
 
-#export PATH="${PATH%%:~/github*}"
 add_PATH() {
     local env_var
     local path
@@ -38,15 +35,15 @@ add_PATH() {
     fi
     path="${path%/}"
     path="${path//[[:space:]]/}"
-    # if ! [[ "${!env_var}" =~ (^|:)${path}(:|$) ]]; then
-    #     # shellcheck disable=SC2140
-    #     eval "${env_var}"="${!env_var}:${path}"
-    # fi
+    if ! [[ "${!env_var}" =~ (^|:)${path}(:|$) ]]; then
+        # shellcheck disable=SC2140
+        eval "${env_var}"="${!env_var}:${path}"
+    fi
     # to prevent Empty compile time value given to use lib at /Users/hari/perl5/lib/perl5/perl5lib.pm line 17.
     #PERL5LIB="${PERL5LIB##:}"
     # fix for Codeship having a space after one of the items in their $PATH, causing the second half of the $PATH to error out as a command
-    # eval "${env_var}"="${!env_var//[[:space:]]/}"
-    # eval "${env_var}"="${!env_var##:}"
+    eval "${env_var}"="${!env_var//[[:space:]]/}"
+    eval "${env_var}"="${!env_var##:}"
     export "${env_var?env_var not defined in add_PATH}"
 }
 
@@ -72,31 +69,21 @@ add_PATH "/sbin"
 add_PATH "/usr/sbin"
 add_PATH "/usr/local/sbin"
 add_PATH "/usr/local/bin"
-add_PATH "/usr/local/opt/python/libexec/bin" # Mac brew installed Python, must be ahead of ~/anaconda/bin below
-add_PATH ~/.jx/bin
-#add_PATH "${JX_HOME:-$HOME/.jx}/bin"
 add_PATH "${bash_tools}"
-add_PATH ~/bin
-# for x in ~/bin/*; do
-#     [[ -d "${x}" ]] || continue
-#     if [[ -d "${x}/bin" ]]; then
-#         add_PATH "${x}/bin"
-#     else
-#         add_PATH "${x}"
-#     fi
-# done
+add_PATH "${HOME}/bin"
+for x in ~/bin/*; do
+    [[ -d "${x}" ]] || continue
+    if [[ -d "${x}/bin" ]]; then
+        add_PATH "${x}/bin"
+    else
+        add_PATH "${x}"
+    fi
+done
 
 # AWS CLI Linux install location
 if [[ -d ~/.local/bin ]]; then
     add_PATH ~/.local/bin
 fi
-
-# AWS SAM CLI Linux install location
-if [[ -d "/home/linuxbrew/.linuxbrew/bin" ]]; then
-    add_PATH "/home/linuxbrew/.linuxbrew/bin"
-fi
-
-add_PATH ~/.pulumi/bin
 
 # do the same with MANPATH
 if [[ -d ~/man ]]; then
@@ -108,38 +95,14 @@ fi
 #                           P a r q u e t   T o o l s
 # ============================================================================ #
 
-for x in ~/bin/parquet-tools-*; do
-    if [[ -d "${x}" ]]; then
-        add_PATH "${x}"
-    fi
-done
+# for x in ~/bin/parquet-tools-*; do
+#     if [[ -d "${x}" ]]; then
+#         add_PATH "${x}"
+#     fi
+# done
 
-if [[ -d /usr/local/parquet-tools ]]; then
-    add_PATH "/usr/local/parquet-tools"
-fi
-
-# ============================================================================ #
-#                         M y   G i t H u b   r e p o s
-# ============================================================================ #
-
-# $github defined in aliases.sh
-# shellcheck disable=SC2154
-# add_PATH "${github}/bash-tools"
-# add_PATH "${github}/bash-tools/applescript"
-# add_PATH "${github}/bash-tools/setup"
-# add_PATH "${github}/go-tools"
-# add_PATH "${github}/go-tools/bin"
-# add_PATH "${github}/perl-tools"
-# add_PATH "${github}/pytools"
-# add_PATH "${github}/tools"
-# #add_PATH "$github/tool"
-# add_PATH "${github}/nagios-plugins"
-# add_PATH "${github}/nagios-plugin-kafka"
-# add_PATH "${github}/spotify"
-# add_PATH "${github}/spotify-tools"
-
-# if is_linux; then
-#     add_PATH ~/.buildkite-agent/bin
+# if [[ -d /usr/local/parquet-tools ]]; then
+#     add_PATH "/usr/local/parquet-tools"
 # fi
 
 # ============================================================================ #
