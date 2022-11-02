40a7f431472bc8fad675f77d24c175c7cba852d7
Kristijan Bakaric <kbakaric1@gmail.com>
2022-11-02 21:53:31 +0100

    remove dedipe

--- home/.bash.d/paths.sh
+++ home/.bash.d/paths.sh
@@ -42,9 +42,9 @@ add_PATH() {
     # to prevent Empty compile time value given to use lib at /Users/hari/perl5/lib/perl5/perl5lib.pm line 17.
     #PERL5LIB="${PERL5LIB##:}"
     # fix for Codeship having a space after one of the items in their $PATH, causing the second half of the $PATH to error out as a command
-    # eval "${env_var}"="${!env_var//[[:space:]]/}"
-    # eval "${env_var}"="${!env_var##:}"
-    #export "${env_var?env_var not defined in add_PATH}"
+    eval "${env_var}"="${!env_var//[[:space:]]/}"
+    eval "${env_var}"="${!env_var##:}"
+    export "${env_var?env_var not defined in add_PATH}"
 }
 
 # use 'which -a'
@@ -71,19 +71,19 @@ add_PATH "/usr/local/sbin"
 add_PATH "/usr/local/bin"
 add_PATH "${bash_tools}"
 add_PATH "${HOME}/bin"
-# for x in ${HOME}/bin/*; do
-#     [[ -d "${x}" ]] || continue
-#     if [[ -d "${x}/bin" ]]; then
-#         add_PATH "${x}/bin"
-#     else
-#         add_PATH "${x}"
-#     fi
-# done
+for x in ${HOME}/bin/*; do
+    [[ -d "${x}" ]] || continue
+    if [[ -d "${x}/bin" ]]; then
+        add_PATH "${x}/bin"
+    else
+        add_PATH "${x}"
+    fi
+done
 
 # AWS CLI Linux install location
-# if [[ -d ~/.local/bin ]]; then
-#     add_PATH ~/.local/bin
-# fi
+if [[ -d ~/.local/bin ]]; then
+    add_PATH ~/.local/bin
+fi
 
 # do the same with MANPATH
 if [[ -d ~/man ]]; then
@@ -134,19 +134,19 @@ fi
 #         done
 # }
 
-dedupe_paths() {
-    local PATH_tmp=""
-    # <( ) only works in Bash, but breaks when sourced from sh
-    # <( ) also ignores errors which don't get passed through the /dev/fd
-    # while read -r path; do
-    #done < <(tr ':' '\n' <<< "$PATH")
-    local IFS=':'
-    for path in ${PATH}; do
-        add_PATH PATH_tmp "${path}"
-    done
-    export PATH="${PATH_tmp}"
-}
+# dedupe_paths() {
+#     local PATH_tmp=""
+#     # <( ) only works in Bash, but breaks when sourced from sh
+#     # <( ) also ignores errors which don't get passed through the /dev/fd
+#     # while read -r path; do
+#     #done < <(tr ':' '\n' <<< "$PATH")
+#     local IFS=':'
+#     for path in ${PATH}; do
+#         add_PATH PATH_tmp "${path}"
+#     done
+#     export PATH="${PATH_tmp}"
+# }
 
-dedupe_paths
+# dedupe_paths
 
 export PATHS_SET=1
