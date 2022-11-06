e91a6092fb24c27a55e88e4d2fb03a7d78dc97ad
Kristijan Bakaric <kbakaric1@gmail.com>
2022-11-06 20:26:10 +0100

    colors.sh

--- home/.bash.d/colors.sh
+++ home/.bash.d/colors.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 #  shellcheck disable=SC2034
 #  vim:ts=4:sts=4:sw=4:et
-#
-#  Author: Hari Sekhon
-#  Date: 2012-06-25 15:20:39 +0100
-#
-#  https://github.com/HariSekhon/DevOps-Bash-tools
-#
-#  License: see accompanying Hari Sekhon LICENSE file
-#
-#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
-#
-#  https://www.linkedin.com/in/HariSekhon
-#
 
 # ============================================================================ #
 #                        Terminal ANSI Escape Color Codes
